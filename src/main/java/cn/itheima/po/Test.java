ee8ac6de9cb3e6558b2248724e56c428f8ac5abf
dee <ad403hd@163.com>
2019-02-24 15:32:22 +0800

    add class Test

---
+++ src/main/java/cn/itheima/po/Test.java
@@ -0,0 +1,10 @@
+package cn.itheima.po;
+
+/*
+create by dee on 2019/2/24
+*/
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
