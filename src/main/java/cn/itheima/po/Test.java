5e1d21f68d173b8a78c01ec70c7050c5bf4e5919
dee <ad403hd@163.com>
2019-02-24 15:37:20 +0800

    add class Test

--- src/main/java/cn/itheima/po/Test.java
+++ src/main/java/cn/itheima/po/Test.java
@@ -5,6 +5,6 @@ create by dee on 2019/2/24
 */
 public class Test {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello world 2222");
     }
 }
